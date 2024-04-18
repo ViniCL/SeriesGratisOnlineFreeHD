82fd14c13769f6292faac2500e4087c770e17a80
ViniCL <52608377+ViniCL@users.noreply.github.com>
2024-04-18 18:09:50 -0300

    refactor: Remoção de campos não mais usados
    Esses 3 campos agora são obtidos dos dados já carregados em memoria.

--- Models/serieinfodto.h
+++ Models/serieinfodto.h
@@ -10,9 +10,6 @@
 class SerieInfoDTO
 {
     Q_GADGET
-    Q_PROPERTY(QString name READ name WRITE setName )
-    Q_PROPERTY(QUrl image READ image WRITE setImage)
-    Q_PROPERTY(double rating READ rating WRITE setRating )
     Q_PROPERTY(QString summary READ summary WRITE setSummary )
     Q_PROPERTY(QStringList genres READ genres WRITE setGenres)
 
@@ -21,26 +18,13 @@ public:
 
     void fillFromSerieStub(const SerieStubResponse& serieStub);
 
-    QString name() const;
-    void setName(const QString& name);
-
-    QUrl image() const;
-    void setImage(const QUrl& image);
-
-    double rating() const;
-    void setRating(double rating);
-
     QString summary() const;
     void setSummary(const QString& summary);
 
     QStringList genres() const;
     void setGenres(const QStringList& genres);
 
-
 private:
-    double _rating;
-    QUrl _image;
-    QString _dsName;
     QString _summary;
     QStringList _genres;
 };
